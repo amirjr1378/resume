5968bb7dcd448ab89aed08afd2c018cd15696b06
amir <jramir669@gmail.com>
2021-05-04 15:23:29 +0430

    add meta desc and meta title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,24 @@ import { withTranslation } from "src/i18n";
 import Header from "src/containers/Header";
 import Main from "src/containers/Main";
 import Footer from "src/containers/Footer";
+import Head from "next/head";
 
 const Homepage = ({ t }) => {
   return (
     <>
+      <Head>
+        <title>Amir karimi Resume</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="keywords"
+          content="resume, React, Reactjs, web, development, js, es5, ecmascript, "
+        />
+
+        <meta
+          name="description"
+          content="resume of an experienced, professional fron end (react js) developer"
+        />
+      </Head>
       <Header />
       <Main />
       <Footer />
