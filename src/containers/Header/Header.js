4d32b14b71ea0cd464aabfe8f56fbb31441f9a55
amir <jramir669@gmail.com>
2021-08-15 18:03:18 +0430

    update

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -3,15 +3,25 @@ import { withTranslation } from "src/i18n";
 import { HashLink as Link } from "src/components/HashLink";
 import withIntersection from "src/components/withIntersection";
 import Navbar from "./Navbar";
+import Typewriter from "typewriter-effect/dist/core";
+import { useEffect } from "react";
 
 function Header({ t }) {
+  useEffect(() => {
+    const descriptionTypeWritter = new Typewriter("#description", {
+      autoStart: true,
+      loop: true,
+    });
+    descriptionTypeWritter.typeString(t("author_description")).start();
+  });
+
   const Hero = withIntersection(
     (props) => {
       return (
         <div className="landing__header__content">
           <h1>{t("author")}</h1>
           <br />
-          <h2>{t("author_description")}</h2>
+          <h2 id="description" />
           <Link
             to="#works"
             style={{ marginTop: 30 }}
